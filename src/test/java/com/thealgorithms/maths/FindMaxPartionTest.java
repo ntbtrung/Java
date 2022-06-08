fdd042c428c0fa9deec79fa6cad127a0cae143ff
ntbtrung <85278958+ntbtrung@users.noreply.github.com>
2022-06-08 10:30:13 +0700

    Update FindMaxPartionTest.java

--- src/test/java/com/thealgorithms/maths/FindMaxPartionTest.java
+++ src/test/java/com/thealgorithms/maths/FindMaxPartionTest.java
@@ -7,18 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FindMaxPartionTest {
     @Test
-    // Array of even numbers
-    public void maxOfArrayEven() {
-        assertEquals(10,FindMax.findMax(new int[] {0,2,4,6,8,10}));
+    // Array in descending order
+    public void maxOfDescendingOrder() {
+        assertEquals(10,FindMax.findMax(new int[] {10,9,8,7,6,5,4,3,2,1}));
     }
+
     @Test
-    // Array of odd numbers
-    public void maxOfArrayOdd() {
-        assertEquals(5,FindMax.findMax(new int []{1,3,5}));
-    }
-    @Test
-    // Array of integer numbers
+    // Array of ascending numbers
     public void maxOfArrayInt() {
-        assertEquals(10,FindMax.findMax(new int[] {0,1,2,3,4,5,6,7,8,9,10}));
+        assertEquals(10,FindMax.findMax(new int[] {1,2,3,4,5,6,7,8,9,10}));
+    }
+    
+    @Test
+    // Array of unsorted array
+    public void maxOfArrayOdd() {
+        assertEquals(8,FindMax.findMax(new int []{1,3,5,4,2,7,8}));
     }
 }
